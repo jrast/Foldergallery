1d5c3491c83ff3f39d8c7ffde9fee5a1f886c180
Juerg Rast <juergr@gmail.com>
2011-03-15 18:54:14 +0100

    Changed add.php to fit the new settings-table

--- add.php
+++ add.php
@@ -10,7 +10,17 @@ if(!defined('WB_PATH')) die(header('Location: index.php'));
  */
 $root_dir = 'd41d8cd98f00b204e9800998ecf8427e';
 $extensions = 'jpg,jpeg,gif,png';
-$database->query("INSERT INTO `" .TABLE_PREFIX ."mod_foldergallery_settings` "
-		. "(`page_id`, `section_id`, `root_dir`, `extensions`) VALUES "
-		. "('$page_id', '$section_id', '$root_dir', '$extensions')");
+
+$rawSql = "INSERT INTO `".TABLE_PREFIX."mod_foldergallery_settings` (`section_id`, `s_name`, `s_value`)"
+    ." VALUES ('".$section_id."', '%s', '%s');";
+
+$database->query(sprintf($rawSql, 'page_id', $page_id));
+$database->query(sprintf($rawSql, 'root_dir', $root_dir));
+$database->query(sprintf($rawSql, 'extensions', $extensions));
+$database->query(sprintf($rawSql, 'invisible', ''));
+$database->query(sprintf($rawSql, 'pics_pp', '15'));
+$database->query(sprintf($rawSql, 'thumb_size', '150'));
+$database->query(sprintf($rawSql, 'ratio', '1'));
+$database->query(sprintf($rawSql, 'catpic', '0'));
+$database->query(sprintf($rawSql, 'lightbox', 'shutter'));
 ?>
