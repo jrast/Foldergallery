2ae08ccc0c45e16a9aa0e552a6a075c4b48f8607
Juerg Rast <juergr@gmail.com>
2011-03-21 12:57:43 +0100

    Added more content to helppage

--- help.php
+++ help.php
@@ -19,7 +19,7 @@ $error = array();
 
 // Check for updates!
 require_once (WB_PATH.'/modules/foldergallery/info.php');
-$versionFeedback = sprintf($FG_HELP['UpToDate'],$module_version);
+$versionFeedback = '';
 $newVersion = @file_get_contents("http://www.foldergallery.ch/version_info.php");
 if($newVersion == false || strlen($newVersion) > 10) {
     $error['version_check'] = $FG_HELP['VERSION_CHECK_ERROR'];
@@ -27,15 +27,15 @@ if($newVersion == false || strlen($newVersion) > 10) {
     $newVersionSplit = explode('.', $newVersion);
     $installedVersionSplit = explode('.', $module_version);
     if(count($newVersionSplit) == count($installedVersionSplit)) {
+        $versionFeedback = $FG_HELP['NOUPDATES'];
         for($i = 0; $i < 2; $i++){
             if($newVersionSplit[$i] > $installedVersionSplit[$i]) {
-                $newVersion = sprintf($FG_HELP['NotUpToDate'], $module_version, $newVersion);
+                $versionFeedback = sprintf($FG_HELP['UPDATE'],$newVersion);
                 break;
             }
         }
     } else {
         $error['version_compare'] = $FG_HELP['VERSION_COMPARE_ERROR'];
-        $newVersion = '...';
     }
 }
 
@@ -62,14 +62,19 @@ if(key_exists('version_compare', $error) || key_exists('version_check', $error))
     $t->clear_var('VersionError');
 }
 
-
-
 $t->set_var(array(
-    'TITLE'             => $FG_HELP['TITLE'],
-    'VERSION'           => $FG_HELP['VERSION'],
-    'VERSION_COMPARE'   => $versionFeedback,
-    'VERSION_TEXT'      => $FG_HELP['VERSION_TEXT'],
-    'HOMEPAGE_TEXT'     => $FG_HELP['HOMEPAGE_TEXT']
+    'TITLE'                 => $FG_HELP['TITLE'],
+    'VERSION'               => $FG_HELP['VERSION'],
+    'YOUR_VERSION_TEXT'     => sprintf($FG_HELP['YOUR_VERSION'],$module_version),
+    'NEWER_VERSION_TEXT'    => $versionFeedback,
+    'VERSION_TEXT'          => $FG_HELP['VERSION_TEXT'],
+    'HOMEPAGE_TEXT'         => $FG_HELP['HOMEPAGE_TEXT'],
+    'HELP_TITLE'            => $FG_HELP['HELP_TITLE'],
+    'HELP_TEXT'             => $FG_HELP['HELP_TEXT'],
+    'BUG_TITLE'             => $FG_HELP['BUG_TITLE'],
+    'BUG_TEXT'              => $FG_HELP['BUG_TEXT'],
+    'BACK_STRING'           => $FG_HELP['BACK_STRING'],
+    'BACK_ONCLICK'          => 'javascript: window.location = \''.ADMIN_URL.'/pages/modify.php?page_id='.$page_id.'\';'
 ));
 
 $t->pparse('output', 'help');
--- help/EN.php
+++ help/EN.php
@@ -1,21 +1,18 @@
 <?php
-
-$FG_HELP['TITLE']           = 'Foldergallery: Hilfe- und Infoseite';
+$FG_HELP['TITLE']           = 'Foldergallery: Help- and Infopage';
 $FG_HELP['VERSION']         = 'Versionsinfo';
-$FG_HELP['YOUR_VERSION']    = 'Sie verwenden Version %s.';
-$FG_HELP['NOUPDATES']       = 'Dies ist momentan die aktuellste Version!';
-$FG_HELP['UPDATE']          = 'Version %s ist verfügbar, es wird empfohlen ein Update durchzuführen.';
+$FG_HELP['YOUR_VERSION']    = 'You are using Version %s.';
+$FG_HELP['NOUPDATES']       = 'There are no updates aviable at the moment!';
+$FG_HELP['UPDATE']          = 'Version %s is aviable, a update is recommeded.';
 $FG_HELP['VERSION_TEXT']    = 'Die aktuellste Version ist immer auf <a href="http://www.websitebakers.com/pages/modules/image-galleries/foldergallery.php" target="_blank">websitebakers.com</a>'
                             .' oder auf <a href="http://www.foldergallery.ch/pages/foldergallery/downloads.php" target="_blank" >foldergallery.ch</a> zu finden!';
 $FG_HELP['HOMEPAGE_TEXT']   = 'Auf <a href="http://www.foldergallery.ch" target="_blank">foldergallery.ch</a> finden sie den gesamten Changelog, &auml;ltere Versionen,'
                             .' Tutorials und einen Showroom, in welchem alle Lighbox-Scripts gezeigt werden!';
-$FG_HELP['HELP_TITLE']      = 'Hilfe und Support';
-$FG_HELP['HELP_TEXT']       = 'Support wird in der Websitebaker Community (Forum) angeboten.';
-$FG_HELP['BUG_TITLE']       = 'Problem melden';
+$FG_HELP['HELP_TITLE']      = 'Help and Support';
+$FG_HELP['HELP_TEXT']       = '';
+$FG_HELP['BUG_TITLE']       = 'Report a bug';
 $FG_HELP['BUG_TEXT']        = 'Falls sie ein Problem oder Fehler im Modul gefunden haben, können sie auf foldergallery.ch eine Meldung hinterlassen';
 
-$FG_HELP['VERSION_CHECK_ERROR']     = 'Es konnten keine Angaben &uuml;ber eine neuere Version abgefragt werden!';
-$FG_HELP['VERSION_COMPARE_ERROR']   = 'Beim vergleichen der Versionen ist ein Fehler aufgetreten';
-
-$FG_HELP['BACK_STRING']     = 'Zur&uuml;ck';
+$FG_HELP['VERSION_CHECK_ERROR']     = 'Could not fetch update informations!';
+$FG_HELP['VERSION_COMPARE_ERROR']   = 'A error occured while comparing the versions.';
 ?>
