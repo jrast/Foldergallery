1b289b351c1c5ec06a23b05fb6c4ae84e8b67b6f
Juerg Rast <juergr@gmail.com>
2011-03-30 00:05:31 +0200

    Edited help/info - Page

--- help/EN.php
+++ help/EN.php
@@ -4,15 +4,14 @@ $FG_HELP['VERSION']         = 'Versionsinfo';
 $FG_HELP['YOUR_VERSION']    = 'You are using Version %s.';
 $FG_HELP['NOUPDATES']       = 'There are no updates aviable at the moment!';
 $FG_HELP['UPDATE']          = 'Version %s is aviable, a update is recommeded.';
-$FG_HELP['VERSION_TEXT']    = 'Die aktuellste Version ist immer auf <a href="http://www.websitebakers.com/pages/modules/image-galleries/foldergallery.php" target="_blank">websitebakers.com</a>'
-                            .' oder auf <a href="http://www.foldergallery.ch/pages/foldergallery/downloads.php" target="_blank" >foldergallery.ch</a> zu finden!';
-$FG_HELP['HOMEPAGE_TEXT']   = 'Auf <a href="http://www.foldergallery.ch" target="_blank">foldergallery.ch</a> finden sie den gesamten Changelog, &auml;ltere Versionen,'
-                            .' Tutorials und einen Showroom, in welchem alle Lighbox-Scripts gezeigt werden!';
+$FG_HELP['VERSION_TEXT']    = 'You can find the newest Version on <a href="http://www.websitebakers.com/pages/modules/image-galleries/foldergallery.php" target="_blank">websitebakers.com</a>'
+                            .' or on <a href="http://www.foldergallery.ch/pages/foldergallery/downloads.php" target="_blank" >foldergallery.ch</a>!';
+$FG_HELP['HOMEPAGE_TEXT']   = 'On <a href="http://www.foldergallery.ch" target="_blank">foldergallery.ch</a> you can find the whole Changelog,'
+                            .' Tutorials and a Showroom, in which all lightbox-scripts are presented!';
 $FG_HELP['HELP_TITLE']      = 'Help and Support';
-$FG_HELP['HELP_TEXT']       = '';
+$FG_HELP['HELP_TEXT']       = 'You get support on the <a href="http://www.websitebaker2.org/forum/index.php/topic,21070.0.html" target="_blank">Websitebaker Community (Forum)</a>.';
 $FG_HELP['BUG_TITLE']       = 'Report a bug';
-$FG_HELP['BUG_TEXT']        = 'Falls sie ein Problem oder Fehler im Modul gefunden haben, können sie auf foldergallery.ch eine Meldung hinterlassen';
+$FG_HELP['BUG_TEXT']        = 'If you have found a Bug or some other curios things, leave me a message on <a href="http://www.foldergallery.ch" target="_blank">foldergallery.ch</a>';
 
-$FG_HELP['VERSION_CHECK_ERROR']     = 'Could not fetch update informations!';
-$FG_HELP['VERSION_COMPARE_ERROR']   = 'A error occured while comparing the versions.';
+$FG_HELP['BACK_STRING']     = 'Back';
 ?>
