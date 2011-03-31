9d249b1044d4b6bb3aba24fef14b9ca71ce097f6
Juerg Rast <juergr@gmail.com>
2011-03-31 17:14:37 +0200

    Replaced $path with WB_PATH where necessary, keep a clean version of $path as backup

--- view.php
+++ view.php
@@ -44,6 +44,7 @@ $ratio = $settings['ratio']; //Pumpi
 $query_pages = $database->query("SELECT link FROM " . TABLE_PREFIX . "pages WHERE page_id = '$page_id' LIMIT 1");
 $page = $query_pages->fetchRow();
 $link = WB_URL . PAGES_DIRECTORY . $page['link'] . PAGE_EXTENSION;
+$path = WB_PATH;
 
 $ergebnisse = array(); // Da drin werden dann alle Ergebnisse aus der DB gespeichert
 $unterKats = array(); // Hier rein kommen die Unterkategorien der aktuellen Kategorie
@@ -105,7 +106,7 @@ if (count($ergebnisse) == 0) {
         $parent_id = $ergebnisse[$i]['id'];
 
         $folder = $root_dir . $cat;
-        $pathToFolder = $path . $folder . '/';
+        $pathToFolder = WB_PATH . $folder . '/';
 
         $bildfilename = 'folderpreview.jpg';
 
@@ -135,8 +136,8 @@ if (count($ergebnisse) == 0) {
             }
         }
 
-        $pathToFolder = $path . $folder . '/';
-        $pathToThumb = $path . $folder . $thumbdir . '/';
+        $pathToFolder = WB_PATH . $folder . '/';
+        $pathToThumb = WB_PATH . $folder . $thumbdir . '/';
         $urlToFolder = $url . $folder . '/';
         $urlToThumb = $url . $folder . $thumbdir . '/';
 
@@ -187,9 +188,8 @@ if (count($bilder) != 0) {
         $folder = $root_dir . $result['parent'] . '/' . $result['categorie'] . '/';
     else
         $folder = $root_dir . $result['parent'] . '/';
-    $pathToFolder = $path . $folder . '/';
-    $pathToThumb = $path . $folder . $thumbdir . '/';
-
+    $pathToFolder = WB_PATH . $folder . '/';
+    $pathToThumb = WB_PATH . $folder . $thumbdir . '/';
     $urlToFolder = $url . $folder;
     $urlToThumb = $url . $folder . $thumbdir1 . '/';
 }
